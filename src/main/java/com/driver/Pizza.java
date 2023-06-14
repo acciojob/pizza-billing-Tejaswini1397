f29f80eca57a1e1ed84da53a52e008a5b7503650
Tejaswini Khairnar <tejaswinikhairnar97@gmail.com>
2023-06-14 09:40:18 +0530

    changes done

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,22 +1,30 @@
 package com.driver;
 
+import com.sun.jdi.PrimitiveValue;
+
 public class Pizza {
 
     private int price;
     private int toppingPrice;
 
+    //private int NonVegToppingPrice;
+
     private int cheesePrice=80;
 
     private  int bagPrice=20;
     private Boolean isVeg;
-    private String bill="Total Price is: ";
+    private String bill;
     private int total;
+    private boolean isCheeseAdded = false;
+    private boolean isToppingsAdded = false;
+    private boolean isTakeAwayAdded = false;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         if(isVeg){
            price=300;
-           toppingPrice=70;
+            toppingPrice=70;
            total=300;
         }else{
             price=400;
@@ -30,20 +38,45 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        total+=cheesePrice;
+        if(!isCheeseAdded){
+            if(isVeg){
+                total+=cheesePrice;
+                isCheeseAdded=true;
+            }
+        }
     }
 
     public void addExtraToppings(){
-        total+=toppingPrice;
+        if(!isToppingsAdded) {
+            if (isVeg) {
+                total +=toppingPrice;
+            }
+            isToppingsAdded=true;
+
+        }
     }
 
     public void addTakeaway(){
-        total+=bagPrice;
+        if(isTakeAwayAdded){
+            total+=bagPrice;
+            isTakeAwayAdded=true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+       this.bill = "Base Price Of The Pizza: " + price + "\n";
 
-        return this.bill+ total;
+        if(isCheeseAdded) // add in bill if he opted for extra cheese,
+            bill = bill + "Extra Cheese Added: " + cheesePrice + "\n";
+
+        if(isToppingsAdded) // add if he opted for extra toppings
+            bill = bill + "Extra Toppings Added: " + toppingPrice+ "\n";
+
+        if(isTakeAwayAdded) // if he is opted for takeaway
+            bill = bill + "Paperbag Added: " + bagPrice + "\n";
+
+        bill = bill + "Total Price: " + total;
+
+        return this.bill;
     }
 }
